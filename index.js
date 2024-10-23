4cb2247d9d3ac8fee8026f1c1fa7e77dbf5fa4e1
Anna Scaria <anna@toagroup.local>
2024-10-23 17:44:41 +0900

    hex validation

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const inValidHex = (hex) => {
+const isValidHex = (hex) => {
     if(!hex) return false;
 
     const strippedHex = hex.replace('#','');
+
+    const hexRegex = /^[0-9a-fA-F]+$/;
+
+    return (strippedHex.length === 3 || strippedHex.length === 6) && hexRegex.test(strippedHex);
 }
+
+console.log(isValidHex("#000000")) //true
+console.log(isValidHex("#0000000")) //false
+console.log(isValidHex("#ffzff")) //true
+console.log(isValidHex("#fff")) //true
+console.log(isValidHex("fff")) //true
+console.log(isValidHex("#bac")) //false
