5eefc0d83b9780de6075ab4ed93870b7acb81547
Anna Scaria <anna@toagroup.local>
2024-10-24 16:57:01 +0900

    altered color displayed

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const hexInput = document.getElementById('hexInput');
 const inputColor = document.getElementById('inputColor');
+const sliderText = document.getElementById('sliderText');
+const slider = document.getElementById('slider');
+const alteredColor = document.getElementById('alteredColor');
+const alteredColorText = document.getElementById('alteredColorText');
 
 hexInput.addEventListener('keyup', () => {
     const hex= hexInput.value;
@@ -34,6 +38,43 @@ const convertHexToRGB = (hex) => {
     const g = parseInt(strippedHex.substring(0,2),16);
     const b = parseInt(strippedHex.substring(0,2),16);
 
-    return {r,g,b}
+    return {r,g,b};
 }
 
+const convertRGBToHex = (r,g,b) => {
+    
+    const firstPair  = ("0" + r.toString(16)).slice(-2);
+    const secondPair = ("0" + g.toString(16)).slice(-2);
+    const thirdPair  = ("0" + b.toString(16)).slice(-2);
+
+    const hex = "#" + firstPair + secondPair + thirdPair;
+    return hex; 
+}
+
+slider.addEventListener('input',() =>{
+    if(!isValidHex(hexInput.value)) return;
+    sliderText.textContent = `${slider.value}%`;
+
+    const alteredHex = alterColor(hexInput.value,slider.value);
+    alteredColor.style.backgroundColor = alteredHex;
+    alteredColorText.innerText = `Altered Color ${alteredHex}%`;
+})
+
+
+const alterColor = (hex,percentage) => {
+    const {r,g,b} = convertHexToRGB(hex);
+
+    const amount = Math.floor((percentage/100) * 255);
+
+    const newR = increaseWithin0To255(r, amount);
+    const newG = increaseWithin0To255(g, amount);
+    const newB = increaseWithin0To255(b, amount);
+    return convertRGBToHex(newR,newG,newB);
+}
+
+const increaseWithin0To255 = (hex,amount) => {
+    const newHex = hex + amount;
+    if(newHex > 255) return 255;
+    if(newHex <0) return 0;
+    return newHex;
+}
