da5392a7ecded828958b23c62bb22fe6336fd55c
Anna Scaria <anna@toagroup.local>
2024-10-24 11:48:25 +0900

    hex to RGB

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const inputColor = document.getElementById('inputColor');
 
 hexInput.addEventListener('keyup', () => {
     const hex= hexInput.value;
+    if(!isValidHex(hex)) return;
+
+    const strippedHex = hex.replace('#','');
+    inputColor.style.backgroundColor = "#"+ strippedHex;
 })
 
 const isValidHex = (hex) => {
@@ -14,3 +18,22 @@ const isValidHex = (hex) => {
 
     return (strippedHex.length === 3 || strippedHex.length === 6) && hexRegex.test(strippedHex);
 }
+
+const convertHexToRGB = (hex) => {
+    if(!isValidHex(hex)) return null;
+
+    let strippedHex = hex.replace("#","");
+
+    if( strippedHex.length === 3){
+        strippedHex = strippedHex[0] + strippedHex[0]
+        + strippedHex[1] + strippedHex[1]
+        + strippedHex[2] + strippedHex[2];
+    }
+
+    const r = parseInt(strippedHex.substring(0,2),16);
+    const g = parseInt(strippedHex.substring(0,2),16);
+    const b = parseInt(strippedHex.substring(0,2),16);
+
+    return {r,g,b}
+}
+
